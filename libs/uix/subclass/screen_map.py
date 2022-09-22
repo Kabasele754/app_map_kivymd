45b264039248d685711f157df181a0411dfb4f3b
Achille kabasele <pepexykabasele@gmail.com>
2022-09-22 05:12:49 +0200

    update contry maps

---
+++ libs/uix/subclass/screen_map.py
@@ -0,0 +1,12 @@
+from kivymd.uix.screen import MDScreen
+from kivy.lang import Builder
+
+# read kivy file 
+Builder.load_file('libs/uix/screen_map.kv')
+
+
+
+class ScreenMaps(MDScreen):
+    """
+    class Screen Maps 
+    """
